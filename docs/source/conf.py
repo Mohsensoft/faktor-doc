ba58619b6cc45871641bce84c7dfcc26aca1afe3
Mohsen <MohsenBahraminejad@yahoo.com>
2018-04-27 23:58:40 +0430

    add icons

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,6 +82,8 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_path = ["_templates/"]
 html_search_language = 'fa'
 
+html_favicon = 'images/icon.ico'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
