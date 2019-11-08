a4e75df283cc17d36ade3c86ba3f78bbf43e6152
Mohsen <MohsenBahraminejad@yahoo.com>
2019-11-08 18:08:24 +0330

    intro b620

--- docs/Create Zip Folder.bat
+++ docs/Create Zip Folder.bat
@@ -1,2 +1,4 @@
-xcopy "build\html\*.*" "Z:\Help\Help_files\" /e /f /h
+xcopy "build\html\*.*" "Z:\Help\Help_files\"
+xcopy "build\html\_images\*.*" "Z:\Help\Help_files\_images\" /e /f /h
+xcopy "build\html\_static\*.*" "Z:\Help\Help_files\_static\" /e /f /h
 copy "Help.html" "Z:\Help\Help.html"
