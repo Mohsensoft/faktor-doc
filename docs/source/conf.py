f85dbb15a428cafb356de669d319be54a4109843
Mohsnb <MohsenBahraminejad@yahoo.com>
2024-09-29 12:55:20 +0330

    fix video frames border

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2024, Mohsensoft'
 author = 'Mohsen Bahraminejad, Mohammad reza khalifeh'
 
 # The short X.Y version
-version = '4.71'
+version = '4.72'
 # The full version, including alpha/beta/rc tags
 release = '.rst'
 
